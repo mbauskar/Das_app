5e07645698f9b8beb708eec13eec47c5116cc2d1
Makarand <mbauskar@gmail.com>
2015-06-26 13:24:45 +0530

    removed the errprint

--- das/das_validations.py
+++ das/das_validations.py
@@ -3,7 +3,6 @@ from datetime import datetime as dt
 
 def delivery_note_validations(doc, method):
 	# start date end end date
-	frappe.errprint(type(doc.start_date))
 	start = dt.strptime(doc.start_date, "%Y-%m-%d %H:%M:%S")
 	end  = dt.strptime(doc.end_date, "%Y-%m-%d %H:%M:%S")
 
