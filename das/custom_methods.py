53fddfd3649538866f5629ae01e7ce85b1ed9fd5
Makarand <mbauskar@gmail.com>
2015-06-27 16:27:43 +0530

    solved PI on save issue

--- das/custom_methods.py
+++ das/custom_methods.py
@@ -7,6 +7,7 @@ def make_purchase_invoice(source_name, target_doc=None):
 	def postprocess(source, target):
 		target.sales_order = source.name
 		target.supplier = source.technician
+		target.credit_to = frappe.db.get_value("Company", frappe.db.get_default("company"), "default_payable_account")
 
 	def update_item(source, target, source_parent):
 		target.amount = flt(source.amount) - flt(source.billed_amt)
@@ -23,8 +24,8 @@ def make_purchase_invoice(source_name, target_doc=None):
 		"Sales Order Item": {
 			"doctype": "Purchase Invoice Item",
 			"field_map": {
-				"name": "po_detail",
-				"parent": "sales_order",
+				# "name": "pr_detail",
+				"parent": "purchase_invoice"
 			},
 			"postprocess": update_item,
 			"condition": lambda doc: frappe.db.get_value("Item",doc.item_code,"is_service_item") == "Yes"
