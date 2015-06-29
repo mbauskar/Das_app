8a19929a48c51850adebbb0b4228267f365a3c69
Makarand <mbauskar@gmail.com>
2015-06-29 17:00:37 +0530

    added 14 days to To date

--- das/das/page/dispatching_dashboar/dispatching_dashboar.js
+++ das/das/page/dispatching_dashboar/dispatching_dashboar.js
@@ -37,7 +37,7 @@ frappe.views.DispachOrderGantt = frappe.views.Gantt.extend({
 			fieldname:"start", "default": frappe.datetime.get_today(), input_css: {"z-index": 3}});
 
 		this.end = this.page.add_field({fieldtype:"Date", label:"To",
-			fieldname:"end", "default": frappe.datetime.get_today(), input_css: {"z-index": 3}});
+			fieldname:"end", "default": frappe.datetime.add_days(frappe.datetime.get_today(),14), input_css: {"z-index": 3}});
 
 		this.technician = this.page.add_field({fieldtype:"Link", label:"Technician",
 			fieldname:"technician", options:"Supplier", get_query:{
