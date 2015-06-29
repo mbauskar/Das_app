514cf67a7aad8afe9bf9d3196da71f084d4a5c62
Makarand <mbauskar@gmail.com>
2015-06-29 19:29:10 +0530

    delivery note validations

--- das/das_validations.py
+++ das/das_validations.py
@@ -6,8 +6,19 @@ def delivery_note_validations(doc, method):
 	start = dt.strptime(doc.start_date, "%Y-%m-%d %H:%M:%S")
 	end  = dt.strptime(doc.end_date, "%Y-%m-%d %H:%M:%S")
 
+	tech_details = is_technician_timeslot_free(start, end, doc.technician)
+
+	if tech_details:
+		frappe.throw("%s is already assigned for other delivery note between given Start Date & End Date"%(doc.technician))
 	if start > end:
 		frappe.throw("End Date should be greater than Start Date")
+	elif start == end:
+		frappe.throw("End Date can not be same as Start Date")
+
+def is_technician_timeslot_free(_from, _to, technician):
+	return frappe.db.sql("""SELECT name FROM `tabDelivery Note` WHERE technician='%s' AND docstatus<>2 AND 
+		(start_date between '%s' AND '%s' OR end_date between '%s' AND '%s')"""%(technician,_from,_to,_from,_to),
+		as_dict=True, debug=1)
 
 def validations_against_batch_number(doc, method):
 	err_items = []
